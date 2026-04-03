f3ae7deb54dbdb27c47567350184bfb0742ab672
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-03 10:10:55 -0600

    Created Project

---
+++ Core.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(UPEAssetSwapper.Core), "UPEAssetSwapper", "1.0.0", "RosePT-10", null)]
+[assembly: MelonGame("Videocult", "Airframe")]
+
+namespace UPEAssetSwapper
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+            LoggerInstance.Msg($"the scene {sceneName} with the... uh... build index? of {buildIndex} was loaded");
+        }
+    }
+}
